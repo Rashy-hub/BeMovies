2c2348c3c5eb9a075477a5e837949b354132de46
Rashy-hub <yagoubi.r@gmail.com>
2024-06-19 23:50:13 +0200

    refacto: migrates all swipe handlers into swipeHandler module

--- javascript/app.js
+++ javascript/app.js
@@ -1,25 +1,10 @@
-import { myFunction } from './swiperHandlers.js'
-import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
+import { swiperOnInit, SwiperFactory } from './swiperHandlers.js'
 
 const searchInput = document.getElementById('search_input')
 const searchSubmit = document.getElementById('search_submit')
 
 const resultsSection = document.getElementById('results')
 const resultSpan = document.querySelector('.results h2 span')
-
-//Swiper events handler
-
-const swiperOnInit = function (event) {
-    if (event.el.classList.contains('swiper-container-result')) {
-        console.log('swiper initialized in results section')
-    } else if (event.el.classList.contains('swiper-container-latest')) {
-        console.log('swiper initialized in latest section')
-        //load default latest images from movie database
-    } else if (event.el.classList.contains('swiper-container-genre')) {
-        console.log('swiper initialized in genre')
-        //load default active genre images from movie database
-    } else console.log('unknown swiper module : default images loaded')
-}
 
 //User interface events handler
 
@@ -58,29 +43,3 @@ const swiperGenre = SwiperFactory(
 swiperResult.enable()
 swiperLatest.enable()
 swiperGenre.enable()
-myFunction()
-
-function SwiperFactory(containerClass, buttonsClass) {
-    const swiper = new Swiper(containerClass, {
-        // Optional parameters
-        direction: 'horizontal',
-        loop: true,
-        slidesPerView: 4,
-        spaceBetween: 19,
-        slidesPerGroup: 1,
-        slidesOffsetAfter: 100,
-        slidesOffsetbefore: 100,
-        //setWrapperSize: true,
-
-        // Navigation arrows with unique class names
-        navigation: {
-            nextEl: `${buttonsClass} .swiper-button-next`,
-            prevEl: `${buttonsClass} .swiper-button-prev`,
-        },
-
-        on: {
-            init: swiperOnInit,
-        },
-    })
-    return swiper
-}
--- javascript/swiperHandlers.js
+++ javascript/swiperHandlers.js
@@ -1,4 +1,41 @@
-// After (ESM)
-export function myFunction() {
-    console.log('I am from a Ecma Script module ')
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
+
+//Swiper events handler
+
+export const swiperOnInit = function (event) {
+    if (event.el.classList.contains('swiper-container-result')) {
+        console.log('swiper initialized in results section')
+    } else if (event.el.classList.contains('swiper-container-latest')) {
+        console.log('swiper initialized in latest section')
+        //load default latest images from movie database
+    } else if (event.el.classList.contains('swiper-container-genre')) {
+        console.log('swiper initialized in genre')
+        //load default active genre images from movie database
+    } else console.log('unknown swiper module : default images loaded')
+}
+
+// Swiper factory that helps create multiple independant swipers
+export function SwiperFactory(containerClass, buttonsClass) {
+    const swiper = new Swiper(containerClass, {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        slidesPerView: 4,
+        spaceBetween: 19,
+        slidesPerGroup: 1,
+        slidesOffsetAfter: 100,
+        slidesOffsetbefore: 100,
+        //setWrapperSize: true,
+
+        // Navigation arrows with unique class names
+        navigation: {
+            nextEl: `${buttonsClass} .swiper-button-next`,
+            prevEl: `${buttonsClass} .swiper-button-prev`,
+        },
+
+        on: {
+            init: swiperOnInit,
+        },
+    })
+    return swiper
 }
